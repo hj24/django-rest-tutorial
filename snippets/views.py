f9e886310ea7e47c30b748218a6f3d3128062a7e
hj24 <huangjian24@foxmail.com>
2019-05-13 17:59:17 +0800

    new tutorial 2 branch

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,5 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
