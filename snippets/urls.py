b0dbeb52405786e63df40d075b2b4708a9718de2
hj24 <huangjian24@foxmail.com>
2019-05-13 18:50:26 +0800

    new tutorial 3 branch

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,8 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^snippets/$', view=views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', view=views.snippet_detail),
+    url(r'^snippets/$', view=views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', view=views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
